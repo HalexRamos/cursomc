9968d640128fceeabcef9b000c3114934fcd6e7f
HalexRamos <halexramos96@gmail.com>
2021-09-07 01:26:55 -0300

    ItemPedido and ItempedidoPK

---
+++ src/main/java/com/halexramos/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,10 @@
+package com.halexramos.cursomc.repositories;
+
+import com.halexramos.cursomc.domain.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}

aa50fb4544cf9d85818cf44853390b5d30a60ef0
HalexRamos <halexramos96@gmail.com>
2021-09-06 21:10:33 -0300

    Client, TypeCLient, phone and address

---
+++ src/main/java/com/halexramos/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,10 @@
+package com.halexramos.cursomc.repositories;
+
+import com.halexramos.cursomc.domain.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}

473a67acf28044268a01980e8ff16b86840b9bbf
HalexRamos <halexramos96@gmail.com>
2021-09-06 22:56:44 -0300

    Request, StatePayment and Payment

---
+++ src/main/java/com/halexramos/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,10 @@
+package com.halexramos.cursomc.repositories;
+
+import com.halexramos.cursomc.domain.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+}
